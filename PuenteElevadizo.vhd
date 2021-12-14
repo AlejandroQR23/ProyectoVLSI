d99fa2afbb896fb636fe12ee073e26fa3f6d5aa6
AlejandroQR23 <alejandro.quijano@comunidad.unam.mx>
2021-12-13 23:21:25 -0600

    Motor de pasos implementado

--- PuenteElevadizo.vhd
+++ PuenteElevadizo.vhd
@@ -1,12 +1,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity PuenteElevadizo is
 	port(
 		clk : in std_logic;
 		
 		buzz   : out std_logic;
+		mot  : out std_logic_vector(3 downto 0);
 		
 		-- Led RGB
 		led_red : out std_logic;
@@ -58,13 +60,35 @@ architecture behavioral of PuenteElevadizo is
 		);
 	end component;
 	
+	component Divisor is
+		Generic 	( N : integer := 24);
+		Port		( clk : in std_logic;
+						div_clk : out std_logic);
+	end component;
+	
+	component Motor is
+		Port	(paso : in std_logic;
+				UD : in std_logic;
+				rst : in std_logic;
+				FH : in std_logic_vector(1 downto 0);
+				MOT : out std_logic_vector(3 downto 0)
+				);
+	end component;
+	
 	signal distanciaEntrada : unsigned(7 downto 0);
 	signal distanciaSalida  : unsigned(7 downto 0);
 	signal inicio				: std_logic := '0';
 	
+	-- Motor
+	signal reloj : std_logic;
+	signal UD : std_logic;
+	signal rst : std_logic;
+	signal FH : std_logic_vector(1 downto 0) := "01";
+	
 	signal senal : integer;
 	
-	constant distanciaDeteccion : unsigned(7 downto 0) := X"0A";	-- distancia a la que se detecta un objeto = 2cm
+	constant distanciaDeteccion : unsigned(7 downto 0)  := X"0A";	  			 -- distancia a la que se detecta un objeto = 2cm
+	constant pasosMax				 : std_logic_vector(11 downto 0) := X"7D0";   -- pasos que dara el motor = 2000
 	
 begin
 
@@ -74,6 +98,9 @@ begin
 	S2 : sonicos port map(clk, inicio, rightTrigger, rightEcho, distanciaSalida); -- sensor de salida
 	
 	L1 : RGB port map(clk, senal, led_red, led_green, led_blue);
+	
+	D1 : divisor generic map (17) port map (clk, reloj);
+	M1 : Motor port map (reloj, UD, rst, FH, mot);
 	
 	-- Detecta entrada de un objeto
 	entraObjeto : process(distanciaEntrada)
@@ -92,6 +119,26 @@ begin
 --			senal <= 0;
 --		end if;
 --	end process;
+
+	mueveMotor : process(clk, UD, rst)
+		variable contador : std_logic_vector(11 downto 0) := X"000";
+	begin
+		if(reloj'event and reloj = '1') then
+			
+			if(senal = 1) then
+				UD <= '1';
+			end if;
+			
+			if(contador < pasosMax) then
+				contador := contador + 1;
+			else
+				contador := X"000";
+				UD <= '0';
+				rst <= '0';
+			end if;
+		
+		end if;
+	end process;
 
 end behavioral;
 
